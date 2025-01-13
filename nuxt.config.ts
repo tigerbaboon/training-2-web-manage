79e62d28a8d916f0ab83c226ec6fd5cdf00eeecc
Tigerbaboon <45224355+tigerbaboon@users.noreply.github.com>
2025-01-13 16:29:20 +0700

    init env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "khonhaen space",
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/icon"],
+  css: ["~/assets/main.css"],
+  runtimeConfig: {
+    public: {
+      webApi: process.env.NUXT_PUBLIC_WEB_API,
+      googleMapsApiKey: process.env.NUXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+  },
+});
